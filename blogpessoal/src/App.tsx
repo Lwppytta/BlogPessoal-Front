cc5647aa647bb40764f674dab88a162290831e1f
Lwppytta <amandalwppytta@gmail.com>
2023-11-06 10:31:36 -0300

    TailwindCSS

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,8 +1,10 @@
 import Home from "./pages/home/Home"
+import "./App.css"
 
 function App() {
     return (
         <>
+            <h1 className="text-green-600">Ola Mundo</h1>
             <Home />
         </>
     )
